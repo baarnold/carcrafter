264eb453b795df51f2c17ae47cd0e1b894e1676f
Benjamin Arnold <kurastwolf@gmail.com>
2016-05-13 14:49:15 -0700

    Update to forms and js for event helpers

--- client/main.js
+++ client/main.js
@@ -1,18 +1,6 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
 
 Template.carchassis.events({
 	'change #sport-chassis' : function(event) {
@@ -54,6 +42,85 @@ Template.carcolor.events({
 		console.log(event.type);
 	},
 	'change #yellow-color': function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	}
+});
+
+Template.carwheel.events({
+	'change #sixteen' : function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	},
+	'change #eighteen': function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	},
+	'change #twenty': function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	},
+	'change #twentytwo': function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	}
+});
+
+Template.carbumper.events({
+	'change #plastic' : function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	},
+	'change #chrome': function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	},
+	'change #carbon': function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	}
+});
+
+Template.carspoiler.events({
+	'change #no-spoil' : function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	},
+	'change #low-spoil': function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	},
+	'change #high-spoil': function(event) {
 		event.preventDefault();
 		
 		//var target = event.target;
