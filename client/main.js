2d9ce9334a2d295ad0cb24138e300a563b41032c
Benjamin Arnold <kurastwolf@gmail.com>
2016-05-13 14:01:58 -0700

    Update to option form

--- client/main.js
+++ client/main.js
@@ -14,9 +14,50 @@ Template.hello.helpers({
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.carchassis.events({
+	'change #sport-chassis' : function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	},
+	'change #suv-chassis': function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	},
+	'change #truck-chassis': function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	}
+});
+
+Template.carcolor.events({
+	'change #blue-color' : function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	},
+	'change #red-color': function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	},
+	'change #yellow-color': function(event) {
+		event.preventDefault();
+		
+		//var target = event.target;
+		//var choice = target.radio.value;
+		console.log(event.type);
+	}
 });
